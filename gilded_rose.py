10ab8bb68966a20e9c9dec6d073ffd778b54aac3
QueCaudroit <quentin.caudroit@free.fr>
2023-03-29 17:00:57 +0200

    implementation switch+ some fixes

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,7 +7,7 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def update_quality_old(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
@@ -37,6 +37,10 @@ class GildedRose(object):
                     if item.quality < 50:
                         item.quality = item.quality + 1
 
+    def update_quality(self):
+        for item in self.items:
+            item.update()
+
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
@@ -44,13 +48,23 @@ class Item:
         self.quality = quality
         if name == "Sulfuras, Hand of Ragnaros":
             self.update_quality = update_sulfuras 
+        elif name == "Aged Brie":
+            self.update_quality = update_brie 
+        elif name == "Backstage passes to a TAFKAL80ETC concert":
+            self.update_quality = update_backstage
+        elif "Conjured" in name:
+            self.update_quality = update_conjured
+        else:
+            self.update_quality = update_default
+
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
     def update(self):
-        self.sell_in -= 1
-        self.quality = self.update_quality(sell_in, quality)
+        if self.name != "Sulfuras, Hand of Ragnaros":
+            self.sell_in -= 1
+        self.quality = self.update_quality(self.sell_in, self.quality)
 
 def update_sulfuras(sell_in, quality):
     return 80
@@ -66,16 +80,19 @@ def update_backstage(sell_in, quality):
         return quality
 
 def update_brie(sell_in, quality):
-    return min(quality + 1, MAX_QUALITY)
+    if sell_in > 0:
+        return min(quality + 1, MAX_QUALITY)
+    else:
+        return min(quality + 2, MAX_QUALITY)
 
 def update_default(sell_in, quality):
-    if sell_in <= 0:
-        return max(quality - 1, MIN_QUALITY)
-    else:
+    if sell_in < 0:
         return max(quality - 2, MIN_QUALITY)
+    else:
+        return max(quality - 1, MIN_QUALITY)
 
 def update_conjured(sell_in, quality):
-    if sell_in <= 0:
-        return max(quality - 2, MIN_QUALITY)
-    else:
+    if sell_in < 0:
         return max(quality - 4, MIN_QUALITY)
+    else:
+        return max(quality - 2, MIN_QUALITY)
